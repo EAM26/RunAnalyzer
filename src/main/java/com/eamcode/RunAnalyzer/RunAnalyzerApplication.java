8b69f983e8a7dbc20e4a1a60f7bc066211ef401d
EAM26 <emuller2607@gmail.com>
2025-06-05 08:23:39 +0200

    First commit.

---
+++ src/main/java/com/eamcode/RunAnalyzer/RunAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.eamcode.RunAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eamcode/RunAnalyzer/RunAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eamcode.RunAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
