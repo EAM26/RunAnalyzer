59cc3759bdb95d76ed20662676bb0ca686ad4903
EAM26 <emuller2607@gmail.com>
2025-06-06 18:27:54 +0200

    Add Phase entity for creating intervals.

---
+++ src/main/java/com/eamcode/RunAnalyzer/model/Phase.java
@@ -0,0 +1,23 @@
+package com.eamcode.RunAnalyzer.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+@Entity
+public class Phase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "report_id")
+    private Report report;
+
+    private LocalTime start;
+    private LocalTime stop;
+    private String category;
+}
