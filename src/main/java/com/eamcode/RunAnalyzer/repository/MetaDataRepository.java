73220d44d1cb122abe28e2053b562994300ee502
EAM26 <emuller2607@gmail.com>
2025-06-11 10:09:05 +0200

    Make Analyzer into util to calculate distance and speed for Phase

--- src/main/java/com/eamcode/RunAnalyzer/repository/MetaDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eamcode.RunAnalyzer.repository;
-
-import com.eamcode.RunAnalyzer.model.MetaData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MetaDataRepository extends JpaRepository<MetaData, Long> {
-}
